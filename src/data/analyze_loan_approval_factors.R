769381fcb738bf594014015313202f67ff3420d6
MihretTe <mtesfay5@students.kennesaw.edu>
2025-07-11 16:11:36 -0400

    Add files via upload

---
+++ src/data/analyze_loan_approval_factors.R
@@ -0,0 +1,18 @@
+# analyze_loan_approval_factors.R
+library(arm)  # for bayesglm
+
+analyze_loan_approval_factors <- function(sim_data) {
+  # Fit Bayesian logistic regression to predict loan approval
+  model <- bayesglm(CanAffordLoan ~ Age + Income + CreditScore,
+                    data = sim_data,
+                    family = binomial(link = "logit"))
+  
+  # Summary of the model
+  print(summary(model))
+  
+  # Confidence intervals
+  conf_int <- confint(model)
+  print(conf_int)
+  
+  return(model)
+}

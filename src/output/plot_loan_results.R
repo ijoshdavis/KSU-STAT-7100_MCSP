7fa45b280a123700c57fa09f5ebfb568fc8808f0
MihretTe <mtesfay5@students.kennesaw.edu>
2025-07-11 16:59:35 -0400

    Update plot_loan_results.R
    Modified Code where each factor can be visualized

--- src/output/plot_loan_results.R
+++ src/output/plot_loan_results.R
@@ -4,18 +4,20 @@
 library(arm)
 library(ggplot2)
 
-# Source all helper scripts
+# Source helper scripts
 source("monte_carlo_simulation.R")
 source("analyze_loan_approval_factors.R")
 source("plot_loan_results.R")
 
-# Run simulation
+# Run simulation to generate data
 sim_data <- run_loan_simulation(n = 1000)
 
-# Analyze data
+# Analyze loan approval factors
 model <- analyze_loan_approval_factors(sim_data)
 
 # Plot results
-print(plot_approval_probability(sim_data))
-print(plot_credit_vs_approval(sim_data))
+#print(plot_approval_probability(sim_data))
+print(plot_Income_vs_approval(sim_data))
+#print(plot_credit_vs_approval(sim_data))  # Correct function name
+
 
